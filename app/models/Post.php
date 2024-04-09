23b7d7fb39684f60911e671cf60a7796dd7d2c83
Darin <dmcfarlane@fordham.edu>
2024-04-09 00:21:43 -0400

    Adding post

--- app/models/Post.php
+++ app/models/Post.php
@@ -16,48 +16,42 @@ class Post
     // Save a new post
     public function savePost($inputData)
     {
-        $query = "INSERT INTO posts (title, content) VALUES (:title, :content)";
-        $statement = $this->pdo->prepare($query);
-        $statement->execute(array(':title' =>  $postData['title'] ,  ':content' => $postData['description']));
-        return $this->pdo->lastInsertId();
+        $query = "insert into posts (title, description) values (:title, :description)";
+        return $this->queryWithParams($query, $inputData);
     }
 
     // Get all Posts
     public function getAllPosts()
     {
     
-        $query = "SELECT * FROM posts";
+        $query = "select * from posts";
         return $this->fetchAll($query);
     }
 
     // Get post by id
     public function getPostById($id)
     {
-        
-        $statement = $this->pdo->prepare("SELECT * FROM posts WHERE id = :id");
-        $statement->execute(array(':id' => $id));
-        return $statement->fetch(PDO::FETCH_ASSOC);
-        
+        $query = "select * from posts where id = :id";
+       return $this-> queryWithParams($query, ['id'=>$id]); 
         
     }
 
     
 
 
-    public function updatePost($id, $title, $content)
+    public function updatePost($inputData)
     {
-        $statement = $this->pdo->prepare("UPDATE posts SET title = :title, content = :content WHERE id = :id");
-       $success= $statement->execute(array(':id' => $id, ':title' => $title, ':content' => $content));
-        return $success;
+        $query = "update posts set title = :title, description = :description where id= :id"; 
+   
+        return $this->queryWithParams($query, $inputData);
     }
 
     // Delete Post By Id
-    public function deletePostById($id)
+    public function deletePost($inputData)
     {
-        $statement = $this->pdo->prepare("DELETE FROM posts WHERE id = :id");
-       $success= $statement->execute([':id'=>$id]);
+        $query = "DELETE FROM posts where id = :id";
    
-       return $success;
+       return $this->queryWithParams($query, $inputData);
     }
 
 
